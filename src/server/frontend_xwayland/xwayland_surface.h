b997bad0f2b9d07e83692d2b8a434eb2341f7dff
William Wold <wm@wmww.sh>
2020-01-31 10:07:18 -0500

    XWaylandSurface::WindowState: add helper methods

--- src/server/frontend_xwayland/xwayland_surface.h
+++ src/server/frontend_xwayland/xwayland_surface.h
@@ -165,6 +165,10 @@ private:
         bool minimized{false};
         bool maximized{false};
         bool fullscreen{false};
+
+        auto operator==(WindowState const& that) const -> bool;
+        auto mir_window_state() const -> MirWindowState;
+        auto updated_from(MirWindowState state) const -> WindowState; ///< Does not change original
     };
 
     /// Overrides from XWaylandSurfaceObserverSurface
